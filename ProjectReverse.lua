c268c4adca61c3f605136502fd14dcfc347abb50
DarkscriptHUBS <yuanagayo01@gmail.com>
2025-07-27 13:20:59 +0800

    Update ProjectReverse.lua

--- ProjectReverse.lua
+++ ProjectReverse.lua
@@ -1,1 +1,3 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/f4shn/main/refs/heads/main/games/gagvisual.lua", true))()
+wait.task(1)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DarkscriptHUBS/wowk/refs/heads/main/loli"))()
